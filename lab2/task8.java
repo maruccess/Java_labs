e26a37c3d8697d45432221be34a08377c3cddd2f
maruccess <135023555+maruccess@users.noreply.github.com>
2025-05-09 17:14:58 +0200

    Create task8.java

---
+++ lab2/task8.java
@@ -0,0 +1,50 @@
+public class RotateMatrix 
+{
+    public static void main(String[] args) 
+    {
+        int[][] matrix = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 16},
+            {17, 18, 19, 20}
+        };
+        
+        int[][] rotatedMatrix = rotateMatrix90Clockwise(matrix);
+
+        System.out.println("Matrix, rotated 90 degrees counterclockwise: ");
+        printMatrix(rotatedMatrix);
+    }
+
+    public static int[][] rotateMatrix90Clockwise(int[][] matrix) 
+    {
+        int row = matrix.length; 
+        int column = matrix[0].length;
+        
+        // in rotated matrix rows change place with columns
+        int[][] rotatedMatrix = new int[column][row];
+
+    for (int i = 0; i < row; i++) 
+    {
+            for (int j = 0; j < column; j++) 
+            {
+                rotatedMatrix[column - 1 - j][i] = matrix[i][j]; 
+            }
+        }
+
+        return rotatedMatrix;
+    }
+    
+    // method for nice matrix output
+    public static void printMatrix(int[][] matrix) 
+    {
+        for (int[] row : matrix) 
+        {
+            for (int value : row) 
+            {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+    }
+}
